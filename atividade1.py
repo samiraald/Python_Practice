74fecafac4db410591501e39ff85bd9e658a5469
Samira <33137790+samiraald@users.noreply.github.com>
2024-11-21 15:20:33 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -1,2 +1,55 @@
-""""1 - Solicite ao usuário que insira um número e, em seguida, use uma estrutura if else para determinar se o número é par ou ímpar."""
+def inicio():
+    mostrarlinha()
+    print ("💥SEJA BEM VINDO AO VALIDADOR PAR OU ÍMPAR💥")
+    mostrarlinha()
 
+def menu():
+    mostrarlinha()
+    print ("1. Começar")
+    print ("2. Sair")
+
+def encerramento():
+    
+    os.system('cls')
+    mostrarlinha()
+    print("Encerrando o Programa")
+    mostrarlinha()
+
+def opcao_invalida():
+     print('Opção Inválida\n')
+     input('Digite uma tecla para retornar a tela principal ') 
+     main()
+
+def selecionando():
+   # try:
+    opcao=int(input("Escolha uma das opções acima: "))
+    if opcao==1:
+        recebe_numero()
+    elif opcao==2:
+        encerramento()
+    else:
+        opcao_invalida()
+    #except Exception as e:
+     #print(str(e))
+
+def validador(entrada):
+    resto = entrada%2
+    if resto==0:
+        print(f'O número {entrada} é PAR')
+    else:
+        print(f'O número {entrada} é ÍMPAR')
+
+def recebe_numero():
+    mostrarlinha()
+    print("Chegou a hora, vamos começar!!!")
+    mostrarlinha()
+    entrada=float(input("Insira o número que deseja validar: "))
+    validador(entrada)
+
+def main():
+    inicio()
+    menu()
+    selecionando()
+
+if __name__ == '__main__':
+    main()
