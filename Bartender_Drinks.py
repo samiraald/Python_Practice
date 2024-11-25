9f4841102cca2266d94c60dbc9d13ab4874186d4
Samira <33137790+samiraald@users.noreply.github.com>
2024-11-25 15:24:45 -0300

    Update Bartender_Drinks.py

--- Bartender_Drinks.py
+++ Bartender_Drinks.py
@@ -46,3 +46,14 @@ def main():
     selecao()
     
 main()
+
+""" Resolvendo de modo mais simples
+def get_drink_by_profession(param):
+    drinks = {"Jabroni": "Patron Tequila",
+                "School Counselor": "Anything with Alcohol",
+                "Programmer": "Hipster Craft Beer",
+                "Bike Gang Member": "Moonshine",
+                "Politician": "Your tax dollars",
+                "Rapper": "Cristal"}
+    return drinks[param.title()] if param.title() in drinks else "Beer"
+"""
